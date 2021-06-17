176de0342c6cefb8142b62cfe8a19bfe437d4f25
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-06-17 07:16:27 +0300

    Added Home page

---
+++ src/components/layout/BasicLayout.vue
@@ -0,0 +1,49 @@
+<template>
+  <ion-header>
+    <ion-toolbar color="primary">
+      <ion-buttons slot="end">
+        <ion-button v-if="!loggedIn" router-link='/login'>
+          <div class="button-container">
+            <ion-icon :icon="personCircleOutline"></ion-icon>
+            <ion-label>Login</ion-label>
+          </div>
+        </ion-button>
+        <ion-button v-else>
+          <div class="button-container">
+            <ion-icon :icon="personCircleOutline"></ion-icon>
+            <ion-label>Logout</ion-label>
+          </div>
+        </ion-button>
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
+    <slot/>
+</template>
+
+<script>
+import {  IonHeader, IonToolbar, IonLabel, IonButton, IonButtons } from '@ionic/vue'
+import { personCircleOutline } from 'ionicons/icons';
+
+export default {
+  components:{  IonHeader, IonToolbar, IonLabel, IonButton, IonButtons},
+  data () {
+    return {
+      loggedIn: false
+    }
+  },
+  setup() {
+    return {personCircleOutline}
+  }
+}
+</script>
+
+<style scoped>
+.button-container{
+  display: grid;
+  grid-template-rows: 70% 30% ;
+  justify-items: center;
+}
+ion-icon{
+  font-size:1.5em
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <ion-page>
+    <basic-layout>
+      <ion-content>
+        <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+          <ion-fab-button>
+            <ion-icon :icon="add"></ion-icon>
+          </ion-fab-button>
+        </ion-fab>
+      </ion-content>
+    </basic-layout>
+  </ion-page>
+</template>
+
+<script>
+import {IonPage, IonContent, IonIcon, IonFab, IonFabButton} from '@ionic/vue'
+import BasicLayout from '../components/layout/BasicLayout.vue'
+import { add } from 'ionicons/icons';
+export default {
+  name: 'Home',
+  components: {
+    IonPage,
+    BasicLayout,
+    IonIcon,
+    IonFab,
+    IonFabButton,
+    IonContent
+  },
+  setup(){
+    return {add};
+  }
+}
+</script>
+
+<style>
+
+</style>
