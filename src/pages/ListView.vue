16974acd281ad67b34e920a9aa50e782208c9510
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-06-25 14:18:04 +0300

    Add new list item and focus on last added item

--- src/pages/ListView.vue
+++ src/pages/ListView.vue
@@ -11,23 +11,38 @@
           </ion-item>
           <list-item
             v-for="(item,index) in listItems"
+            ref="listItem"
             :key="item.id"
             :item="item"
             :index="index"
           />
         </ion-list>
+        <div class="add-item">
+          <ion-button
+            @click="add()"
+            fill="clear"
+            size="large">
+            <ion-icon
+              slot="icon-only"
+              :icon="addCircleOutline"
+            ></ion-icon>
+          </ion-button>
+        </div>
         <hr v-if="completedItems.length > 0">
-          <ion-list v-show="completedItems.length >0">
-            <div class="ion-padding-start">
-              <ion-label>Completed items</ion-label>
-            </div>
-            <list-item
-              v-for="(item,index) in completedItems"
-              :key="item.id"
-              :item="item"
-              :index="index"
-            />
-          </ion-list>
+        <ion-list
+          ref="list"
+          v-show="completedItems.length >0"
+        >
+          <div class="ion-padding-start">
+            <ion-label>Completed items</ion-label>
+          </div>
+          <list-item
+            v-for="(item,index) in completedItems"
+            :key="item.id"
+            :item="item"
+            :index="index"
+          />
+        </ion-list>
       </ion-content>
     </list-view-layout>
   </ion-page>
@@ -36,18 +51,34 @@
 <script>
 import ListViewLayout from '../components/layout/ListViewLayout.vue'
 import ListItem from '../components/ListItem.vue'
-import { IonItem, IonInput, IonPage, IonList, IonContent,IonLabel } from '@ionic/vue'
-import { trashBinSharp } from 'ionicons/icons';
-import { mapState } from 'vuex'
+import { IonItem, IonInput, IonPage, IonList, IonContent, IonLabel, IonIcon, IonButton } from '@ionic/vue'
+import { addCircleOutline } from 'ionicons/icons';
+import { mapState, mapMutations } from 'vuex'
 export default {
   name: 'ListView',
   components: {
-    ListViewLayout, IonPage, IonList, IonContent, ListItem, IonItem, IonInput,IonLabel
+    ListViewLayout, IonPage, IonList, IonContent, ListItem, IonItem, IonInput, IonLabel, IonIcon, IonButton
   },
   setup () {
     return {
-      trashBinSharp
+      addCircleOutline
     }
+  },
+  methods: {
+    ...mapMutations(['addItem']),
+    add () {
+      const item = {
+        "id": 16,
+        "text": '',
+        "isCompleted": false,
+        "listId": 1
+      }
+      this.addItem(item)
+      setTimeout(() => {
+        this.$nextTick(() => this.$refs.listItem.$el.children[1].setFocus());
+      }, 10);
+    }
+
   },
   computed: {
     ...mapState(['shoppingItems']),
@@ -65,5 +96,10 @@ export default {
 .list-title {
   font-weight: bold;
   --placeholder-font-weight: bold;
+}
+.add-item {
+  display: grid;
+  grid-template-columns: auto;
+  justify-items: center;
 }
 </style>
