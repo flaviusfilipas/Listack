605e8927196988ed05922afb3c96729bbc3026c4
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-08-08 15:46:27 +0300

    Added register page.

---
+++ src/pages/Register.vue
@@ -0,0 +1,65 @@
+<template>
+  <ion-page>
+    <basic-layout>
+      <ion-content>
+        <ion-item class="with-margins" lines="full">
+          <ion-label position="floating">Name</ion-label>
+          <ion-icon class="pointer-cursor ion-margin-top" slot="end" :icon="personOutline" color="primary"></ion-icon>
+          <ion-input v-model="name" type="text" required></ion-input>
+        </ion-item>
+        <ion-item class="with-margins" lines="full">
+          <ion-label position="floating">Email</ion-label>
+          <ion-icon class="pointer-cursor ion-margin-top" slot="end" :icon="mailOutline" color="primary"></ion-icon>
+          <ion-input v-model="email" type="email" required></ion-input>
+        </ion-item>
+        <ion-item class="with-margins" lines="full">
+          <ion-label position="floating">Password</ion-label>
+          <ion-icon @click="isPwd = !isPwd" class="pointer-cursor ion-margin-top" slot="end"
+                    :icon="isPwd ? eyeOffOutline: eyeOutline"
+                    color="primary"></ion-icon>
+          <ion-input v-model="password" :type="isPwd? 'password' : 'text'" required>
+          </ion-input>
+        </ion-item>
+        <ion-row class="with-margins">
+          <ion-col>
+            <ion-button shape="round" color="primary" expand="block">Register</ion-button>
+          </ion-col>
+        </ion-row>
+      </ion-content>
+    </basic-layout>
+  </ion-page>
+</template>
+
+<script>
+import {IonPage, IonIcon, IonItem, IonRow, IonCol, IonContent, IonButton, IonInput, IonLabel} from '@ionic/vue'
+import BasicLayout from '../components/layout/BasicLayout.vue'
+import {eyeOffOutline, eyeOutline, mailOutline,personOutline} from "ionicons/icons";
+export default {
+  name: "Register",
+  components: {
+    IonPage, IonIcon, IonItem, IonRow, IonCol, IonContent, IonButton, IonInput, IonLabel, BasicLayout
+  },
+  data() {
+    return {
+      email:'',
+      password:'',
+      name:'',
+      isPwd: true
+    }
+  },
+  methods:{
+
+  },
+  setup() {
+    return {
+      eyeOffOutline, eyeOutline, mailOutline,personOutline
+    }
+  }
+}
+</script>
+
+<style scoped>
+.with-margins {
+  margin: 16px 16px 16px 16px;
+}
+</style>
