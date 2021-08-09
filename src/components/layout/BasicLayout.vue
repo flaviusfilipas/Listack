daace7fe6089182270bbf5183d8c539576c29ac5
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-08-09 13:47:43 +0300

    Added capacitor storage dependency.

--- src/components/layout/BasicLayout.vue
+++ src/components/layout/BasicLayout.vue
@@ -2,18 +2,7 @@
   <ion-header>
     <ion-toolbar color="primary">
       <ion-buttons slot="end">
-        <ion-button v-if="!loggedIn" router-link='/login'>
-          <div class="button-container">
-            <ion-icon :icon="personCircleOutline"></ion-icon>
-            <ion-label>Login</ion-label>
-          </div>
-        </ion-button>
-        <ion-button v-else>
-          <div class="button-container">
-            <ion-icon :icon="personCircleOutline"></ion-icon>
-            <ion-label>Logout</ion-label>
-          </div>
-        </ion-button>
+        <header-auth-button></header-auth-button>
       </ion-buttons>
     </ion-toolbar>
   </ion-header>
@@ -21,29 +10,12 @@
 </template>
 
 <script>
-import {  IonHeader, IonToolbar, IonLabel, IonButton, IonButtons, IonIcon } from '@ionic/vue'
-import { personCircleOutline } from 'ionicons/icons';
-
+import {  IonHeader, IonToolbar, IonButtons,  } from '@ionic/vue'
+import HeaderAuthButton from "@/components/buttons/HeaderAuthButton";
 export default {
-  components:{  IonHeader, IonToolbar, IonLabel, IonButton, IonButtons, IonIcon},
-  data () {
-    return {
-      loggedIn: false
-    }
-  },
-  setup() {
-    return {personCircleOutline}
-  }
+  components:{HeaderAuthButton,  IonHeader, IonToolbar, IonButtons},
 }
 </script>
 
 <style scoped>
-.button-container{
-  display: grid;
-  grid-template-rows: 70% 30% ;
-  justify-items: center;
-}
-ion-icon{
-  font-size:1.5em
-}
 </style>
