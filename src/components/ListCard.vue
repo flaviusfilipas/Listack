07e78f226ba82380828fc767baa0687375e5d0c1
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-08-17 20:44:53 +0300

    Refactored list card. Removed popover.

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -3,21 +3,62 @@
       button
       @click="() => this.$router.push(`/list/${list.id}`)">
     <ion-label>{{ list.title }}</ion-label>
-    <list-details-popover :list="list"></list-details-popover>
+    <ion-button
+        fill="clear"
+        color="danger"
+        @click.stop="deleteList">
+      <ion-icon
+          slot="icon-only"
+          :icon="trashOutline"/>
+    </ion-button>
   </ion-item>
 </template>
 
 <script>
-import {IonLabel, IonItem} from '@ionic/vue'
-import ListDetailsPopover from '../components/popover/ListDetailsPopover.vue'
+import {IonLabel, IonItem, alertController, toastController} from '@ionic/vue'
+import {mapActions, mapMutations} from "vuex";
+import { trashOutline } from 'ionicons/icons';
 
 export default {
   name: 'ListCard',
-  components: {IonLabel, IonItem, ListDetailsPopover},
+  components: {IonLabel, IonItem},
   props: {
     list: {
       type: Object,
       required: true
+    }
+  },
+  methods: {
+    ...mapActions('lists', {deleteListAction: 'deleteList'}),
+    ...mapMutations('lists', {deleteListMutation: 'deleteList'}),
+    deleteList() {
+      alertController.create({
+        header: 'Confirm',
+        message: 'Are you sure you want to delete this list?',
+        buttons: [
+          {
+            text: 'Confirm',
+            handler: () => {
+              this.deleteListAction(this.list.id)
+                  .then(() => {
+                    this.deleteListMutation(this.list.id)
+                    toastController.create({
+                      message: 'List has been deleted',
+                      duration: 1000,
+                    }).then(toast => toast.present())
+                  })
+            },
+          },
+          {
+            text: 'Cancel'
+          },
+        ],
+      }).then(alert => alert.present());
+    }
+  },
+  setup() {
+    return {
+      trashOutline
     }
   },
 }
