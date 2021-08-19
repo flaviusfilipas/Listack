b62f4d687f3cddc6f4f95f2ff430172abeed4471
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-08-19 19:56:10 +0300

    Set current logged in user.

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,46 +1,59 @@
 /* eslint-disable */
 import {auth} from '@/firebase'
 import {Storage} from "@capacitor/storage";
+import axios from 'axios'
 
 const state = () => ({
-    loggedIn: false
+    loggedIn: false,
+    currentUser: {}
 })
 const actions = {
     registerUser({}, payload) {
         return auth.createUserWithEmailAndPassword(payload.email, payload.password)
     },
-    loginUser({}, payload){
-        return auth.signInWithEmailAndPassword(payload.email,payload.password);
+    loginUser({}, payload) {
+        return auth.signInWithEmailAndPassword(payload.email, payload.password);
     },
-    logoutUser(){
+    logoutUser() {
         return auth.signOut()
     },
-    handleAuthStateChange ({ commit }) {
+    getUserById({commit}, userId) {
+        axios.get(`/api/users/${userId}`)
+            .then(response => {
+                commit('setCurrentUser', response.data)
+            });
+    },
+    handleAuthStateChange({commit, dispatch}) {
         auth.onAuthStateChanged(user => {
             if (user) {
                 commit('setLoggedIn', true)
-                Storage.set({key:'loggedIn',value:'true'}).catch(error =>{
-                    console.log("Could not set loggedIn ",error);
+                dispatch("getUserById", user.uid)
+                Storage.set({key: 'loggedIn', value: 'true'}).catch(error => {
+                    console.log("Could not set loggedIn ", error);
                 })
-                Storage.set({key:'userId',value:user.uid}).catch(error =>{
-                    console.log("Could not set userId ",error);
+                Storage.set({key: 'userId', value: user.uid}).catch(error => {
+                    console.log("Could not set userId ", error);
                 })
             } else {
                 commit('setLoggedIn', false)
-                Storage.set({key:'loggedIn',value:'false'}).catch(error =>{
-                    console.log("Could not set loggedIn",error);
+                commit('setCurrentUser', null)
+                Storage.set({key: 'loggedIn', value: 'false'}).catch(error => {
+                    console.log("Could not set loggedIn", error);
                 })
-                Storage.set({key:'userId',value:''}).catch(error =>{
-                    console.log("Could not set userId ",error);
+                Storage.set({key: 'userId', value: ''}).catch(error => {
+                    console.log("Could not set userId ", error);
                 })
             }
         })
     },
 }
 const mutations = {
-    setLoggedIn(state,value){
-        state.loggedIn=value;
-    }
+    setLoggedIn(state, value) {
+        state.loggedIn = value;
+    },
+    setCurrentUser(state, currentUser) {
+        state.currentUser = currentUser;
+    },
 }
 const getters = {}
 
