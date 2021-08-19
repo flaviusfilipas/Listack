556478e5c2d19ab503a223221fe78e833b519da8
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-08-19 17:44:09 +0300

    Added missing imports.

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -15,13 +15,13 @@
 </template>
 
 <script>
-import {IonLabel, IonItem, alertController, toastController} from '@ionic/vue'
+import {IonLabel, IonItem, alertController, toastController, IonIcon} from '@ionic/vue'
 import {mapActions, mapMutations} from "vuex";
 import { trashOutline } from 'ionicons/icons';
 
 export default {
   name: 'ListCard',
-  components: {IonLabel, IonItem},
+  components: {IonLabel, IonItem,IonIcon},
   props: {
     list: {
       type: Object,
