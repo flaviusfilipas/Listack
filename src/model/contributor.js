a796073fd0180e019acf4bd196dee9c739b52b3d
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-08-21 11:22:43 +0300

    Removed unused model.

--- src/model/contributor.js
+++
@@ -1,8 +0,0 @@
-export default class Contributor{
-    constructor(listId,inviterName,contributorEmail){
-        this.id = null
-        this.listId=listId;
-        this.inviterName=inviterName;
-        this.contributorEmail=contributorEmail;
-    }
-}
